fdc0c5c2909d3b3f9dc4eb98fa70e5eaff59a828
mchl <bmnn.mchl@gmail.com>
2014-04-17 17:11:17 +0200

    An intermediate version of my report using Pythontex to include dynamically generated formula.

--- compileLatex.py
+++ compileLatex.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 from os import system
 from time import strftime
-fn = 'fp.tex'
-cmds = ['pdflatex %s > %s',
+fn = 'clean.tex'
+cmds = ['pdflatex -interaction=nonstopmode %s > %s',
 "C:\Anaconda\python.exe  C:\Users\Murad\pythontex\pythontex\pythontex.py %s > %s",
-'pdflatex %s > %s']
+'pdflatex -interaction=nonstopmode %s > %s']
 for c in cmds: 
 	cmd = c %(fn, 'log.swp')
 	print strftime('%d %b %Y %H:%M:%S: ') + cmd 
