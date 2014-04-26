8bd5f267db2f29d28f2923b59f754c182677665d
mchl <bmmnn.mchl@gmail.com>
2014-04-26 10:17:34 +0200

    Implemented programming tasks.

--- oicel/Chapter1/test_waterfilling_cvx.m
+++ oicel/Chapter1/test_waterfilling_cvx.m
@@ -9,24 +9,32 @@ zeta = 1e-2;
 ok = true;
 disp('Testing waterfilling_cvx')
 try
-    for t=[1,2,4] %Not testing the cvx solution with the biggest system. 
+    for t=[1,2,4] %Not testing the cvx solution with the biggest system.
         for i=1:length(testSetsWF(t).P)
+            % TODO What's the matter with test 2.3 ?
+            %     for t=[2] 
+            %        for i=3
+            % Seems like I should not assign any power to c_1?
+            fprintf('Running test %d.%d\n', t, i)
             [R,p,mu] = waterfilling_cvx(testSetsWF(t).h,testSetsWF(t).s,testSetsWF(t).P(i));
             if max(abs(testSetsWF(t).R(i)-R))>zeta
                 display(['FAIL, wrong rate, test No. ' num2str(t) ', with P idx' num2str(i)])
-                keyboard
+                fprintf('Should be: %f  \t is \t %f \n', testSetsWF(t).R(i), R)
                 ok=false;
             end
             if max(abs(testSetsWF(t).p(:,i)-p))>zeta
                 display(['FAIL, wrong powers, test No. ' num2str(t) ', with P idx ' num2str(i)])
-                keyboard
+                fprintf('Should be \t Is\n')
+                [testSetsWF(t).p(:,i), p] %#ok<NOPRT>
                 ok=false;
+                fprintf('\tFailed\n')
+                continue
             end
             if max(abs(testSetsWF(t).mu(i)-mu))>zeta
                 display(['FAIL, wrong Lagrangian multiplier, test No. ' num2str(t) ', with P idx ' num2str(i)])
-                keyboard
                 ok=false;
             end
+            fprintf('\tPassed\n')
         end
     end
 catch e
@@ -36,4 +44,5 @@ catch e
 end
 if ok
     disp('OK')
+    fprintf('--- All tests passed ---\n\n')
 end
--- oicel/Chapter1/waterfilling_cvx.m
+++ oicel/Chapter1/waterfilling_cvx.m
@@ -1,16 +1,17 @@
-function [R,p,mu] = waterfilling_cvx(h,s,P)
-%function [R,p,mu] = waterfilling_cvx(h,s,P)
+function [R,p,mu] = waterfilling_cvx(h,sigma,P)
+% 140426090339
+%function [R,p,mu] = waterfilling_cvx(h,sigma,P)
 %
 % Calculation of the optimal solution to the following sum rate
 % maximization problem under a limited sum power budget.
-%     R =    max sum(log2(1+|hi|^2/si*pi) 
+%     R =    max sum(log2(1+|h_i|^2/sigma_i*pi) 
 %            s.t.:   pi >= 0   for all i
 %                    sum(pi) <= P
-% The sulution is calculated via CVX.
+% The solution is calculated via CVX.
 %
 % Inputs
-% h: vecotr of channel coefficients h1,...,hn
-% s: vecotr of noise variances sigma1^2,...,sigman^2
+% h: vector of channel coefficients h1,...,hn
+% sigma: vector of noise variances sigma1^2,...,sigman^2
 % P: available sum transmit power P
 % Outputs
 % R: value of the maximal sum rate R
@@ -18,9 +19,22 @@ function [R,p,mu] = waterfilling_cvx(h,s,P)
 % mu: value of th optimal Lagrangian multiplier mu
 
 h = h(:);
-s = s(:);
-
+sigma = sigma(:);
+gamma = (abs(h) .^ 2 ) ./ (sigma .^2);
 % determine n
-n = length(h);
-
-% TODO
+n = length(h); %#ok<NASGU>
+%#ok<*NOPRT>
+cvx_begin 
+	cvx_quiet true % no screen outpunt
+    cvx_solver SDPT3 % select solver
+    variable p(n)
+    maximize(sum_log(1 + gamma .* p))
+    subject to
+		sum(p) <= P
+        p >= 0 
+cvx_end
+R = cvx_optval/log(2);
+[p_, i] = max(p);
+assert(p_ > 0)
+mu = 1/(p_ + 1/gamma(i))/log(2);
+end
