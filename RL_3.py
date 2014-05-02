983dc069255c221d4d8f62f5fdf364e8c1dcf16d
mchl <bmnn.mchl@gmail.com>
2014-05-02 09:12:34 +0200

    Got a better version of the book.

--- RL_3.py
+++
@@ -1,9 +0,0 @@
-from sympy import Symbol
-# _sympy.py
-from _sympy import _idx
-k = Symbol('k') # Step index
-r = Symbol('r') # reward
-r_k = _idx(Symbol('r'), k) 
-Q_k = _idx(Symbol('Q'), k) # Average of the first k rewards
-alpha = Symbol('alpha') # step size parameter
-
