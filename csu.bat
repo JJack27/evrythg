54087d064523be02dd0f38006a2c35f54e4870a1
mchl <bmnn.mchl@gmail.com>
2014-05-02 08:40:19 +0200

    Adapted as popen can handle no "" in path.

--- csu.bat
+++ csu.bat
@@ -2,6 +2,8 @@
 echo Running startup script:
 echo %0 
 doskey ed=gvim --remote-silent $*
-set PATH=%PATH%;C:\Users\mchl\Downloads\portableApps\JPEGView64\;C:\Users\mchl\Downloads\portableApps\MiniCap
-echo System info:
+if "%COMPUTERNAME%" EQU  "FP-SURFBRETT-2" goto FP_SURFBRETT_2
+:FP_SURFBRETT_2
+set PATH=%PATH%;C:\Users\Murad\Desktop\portableApps\MiniCapPortable;C:\Users\Murad\Desktop\portableApps\JPEGView64
+:NEXT echo System info:
 cmd
