741ff5fafe5ab04365417957f77e63148269e635
mchl@win <bmnn.mchl@gmail.com>
2014-05-07 08:02:28 +0200

    A more versatile approach to latex printing with sympy.

--- _sympy.py
+++ _sympy.py
@@ -1,9 +1,8 @@
 from sympy import Symbol
 from sympy import abc
+from sympy.printing import latex
 # tex_magic.py
 from tex_magic import TeX1 
-
-
 def _s(expr):
 	"""
 	Show Latex in QT Console
@@ -31,3 +30,30 @@ def _d(v):
 	Variable: v
 	"""	
 	return Symbol(r'\operatorname{d} %s' %(v))
+
+class _latex:
+    """
+    Print a latex representation with special symbols replaced
+    symbol_names: the dictionary containing replacements
+    """
+    def __init__(self):
+        self.symbol_names = dict() 
+    def out(self, expr):
+        """
+        expr: A sympy expression
+        _l = _latex()
+        _p = _l.out
+        _d = _l.symbol_names
+        from sympy import MatrixSymbol
+        from sympy import Symbol
+        from sympy import Matrix
+        X = MatrixSymbol('X', 3, 3)
+        _d[X]=r'\Phi_W'
+        _p(Matrix([[X], [X]]))
+        k = Symbol('k')
+        _d[k]=r'\k_W'
+        _p(Sum(X * k, (k, 1, 10))
+        """
+        return latex(expr, symbol_names=self.symbol_names)
+
+
