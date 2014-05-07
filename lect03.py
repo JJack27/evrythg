08913cb67b28925b57ac1cf66fa2e108a40dd4f9
mchl@win <bmnn.mchl@gmail.com>
2014-05-07 09:22:09 +0200

    Reviewing stuff for yesterdays lecture (in the book).

--- lect03.py
+++
@@ -1,6 +0,0 @@
-# cf. lect02.py
-G = Function('G'), 'Return'
-gamma = Symbol(r'\gamma'), 'Discount rate'
-v = Function('v')(s), 'State-value function'
-q = Function('q')(s,a), 'Action-value function'
-p = Function('p')(s, a), 'cf. 140429175902'
