26d31265d46c6bcdc77d89a7bce800cfaacf6eab
mchl@win <bmnn.mchl@gmail.com>
2014-05-22 00:59:17 +0200

    Miscanellous.

---
+++ htf.py
@@ -0,0 +1,10 @@
+from struct import unpack
+"""
+Parse Surfboard log files
+Hex \to Float
+"""
+with open("2014_05_141_15_05_285.txt") as f:
+    for l in f:
+        v = [v.replace('0x', '').strip() for  v in l.split(',')]
+        if len(v[-1]) < 4: continue
+        print ('%s \t %s \t %+f)' %(v[0], v[2], unpack('<f', v[-1].decode('hex'))[0] * 60))
